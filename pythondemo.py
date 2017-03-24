c06bce35ccbf76dfe4321ebd1a33217b678fb527
yangtao <yangtao@yupaopao.cn>
2017-03-24 15:29:48 +0800

    ..

--- pythondemo.py
+++ pythondemo.py
@@ -4,27 +4,33 @@ from io import BytesIO
 from wordcloud import WordCloud
 import urllib.request
 import jieba
+import re
+import numpy as np
+from PIL import Image
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    fp = urllib.request.urlopen("http://www.python.org")
+    fp = urllib.request.urlopen("http://www.en8848.com.cn/article/beauty/health/71449.html")
     mybytes = fp.read()
-    # note that Python3 does not read the html code as string
-    # but as html code bytearray, convert to string with
     mystr = mybytes.decode("utf8")
+    print(mystr)
     fp.close()
     wordlist_after_jieba = jieba.cut(mystr, cut_all=True)
     wl_space_split = " ".join(wordlist_after_jieba)
-    my_wordcloud = WordCloud().generate(wl_space_split)
+    # abel_mask = np.array(Image.open("c:/2.jpg"))
+    wc = WordCloud()
+    my_wordcloud = wc.generate(wl_space_split)
     plt.imshow(my_wordcloud)
+    # plt.fill()
     plt.axis("off")
     img = BytesIO()
-    plt.savefig(img, dpi=324)
+    plt.savefig(img, dpi=314)
     img.seek(0)
     return Response(img, mimetype='image/png')
+
 
 if __name__ == '__main__':
     app.run()
